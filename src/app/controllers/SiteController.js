72d40720a174587e1ee42857aa9dd12580ee484b
hoang_anh <ledinhhoanganh10@gmail.com>
2024-09-17 22:27:45 +0700

    route & db

---
+++ src/app/controllers/SiteController.js
@@ -0,0 +1,21 @@
+const Course = require('../models/Course');
+
+class SiteController {
+   // [GET] /
+   async index(req, res) {
+      try {
+         const courses = await Course.find({});
+         res.json(courses);
+         res.render('home');
+      } catch (error) {
+         res.status(400).json({ error: 'error' });
+      }
+   }
+
+   // [GET] /search
+   search(req, res) {
+      res.render('search');
+   }
+}
+
+module.exports = new SiteController();
